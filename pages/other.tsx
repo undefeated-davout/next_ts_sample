ea079f0e1c6c462951b3980c7c37d169ca0fbf8d
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2021-06-13 16:58:14 +0900

    add: otherページ

--- pages/other.tsx
+++ pages/other.tsx
@@ -1,8 +1,8 @@
 import Header from './header';
 import Link from 'next/link';
 
-export default function Home() {
-  const title = 'Index';
+export default function Other() {
+  const title = 'Other';
 
   return (
     <div>
@@ -10,10 +10,10 @@ export default function Home() {
       <h1 className="bg-primary px-3 text-white display-4">React</h1>
       <div className="container">
         <h3 className="my-3 text-primary text-center">{title}</h3>
-        <div className="card p-3 text-center">
-          <p>これは、ページ移動のサンプルです。</p>
-          <Link href="/other">
-            <a>Go to Other page &gt;&gt;</a>
+        <div className="card p-3">
+          <p>これは、もう１つのページの表示です。</p>
+          <Link href="/">
+            <a>&lt;&lt; Back to Index page</a>
           </Link>
         </div>
       </div>

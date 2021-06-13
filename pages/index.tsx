934094fc2974b3b51925ff3d9c499bdefa5e9a4a
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2021-06-13 16:54:58 +0900

    refactor: headerをコンポーネント化

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,16 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Header from './header';
 
 export default function Home() {
-  let title = 'Next.js page';
+  let title = 'Next.js Index';
   let message = 'React Next.js sample page.';
 
   return (
     <div>
-      <Head>
-        <title>{title}</title>
-        <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-          crossOrigin="anonymous"
-        ></link>
-      </Head>
-      <style jsx>{`
-        h1 {
-          text-align: center;
-        }
-
-        h2 {
-          text-align: center;
-          margin: 0px 5px;
-          color: #aad;
-          font-size: 36pt;
-          font-weight: bold;
-        }
-
-        p {
-          text-align: left;
-          margin: 0px 5px;
-          color: blue;
-          font-size: 18pt;
-        }
-      `}</style>
-
-      <h1 className="bg-primary text-white display-4 ">React</h1>
+      <Header title={title} />
+      <h1 className="bg-primary px-3 text-white display-4 text-right">React</h1>
       <div className="container">
-        <h2 className="my-3 subtitle">{title}</h2>
-        <div className="alert alert-primary text-center">
+        <h3 className="my-3 text-primary text-center">{title}</h3>
+        <div className="alert alert-primary text-left">
           <p className="h5">{message}.</p>
         </div>
       </div>

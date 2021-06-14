3c19cbf03abfaefac226b973d13abe8eef619e7d
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2021-06-15 03:19:38 +0900

    add: JSON取得

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,46 @@
+import { useState } from 'react';
 import Layout from '../components/layout';
-import MyImage from '../components/image';
+
+type DataType = {
+  message: string;
+  data: {
+    name: string;
+    mail: string;
+    age: number;
+  }[];
+};
 
 export default function Home() {
+  const url = './data.json';
+  const [data, setData] = useState<DataType>({ message: '', data: [] });
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => setData(res));
+
   return (
     <div>
       <Layout header="Next.js" title="Top page.">
         <div className="alert alert-primary text-center">
-          <h5 className="mb-4">Welcome to next.js!</h5>
-          <MyImage fname="image.jpg" size="300" />
+          <h5 className="mb-4">{data.message}</h5>
+          <table className="table bg-white">
+            <thead className="table-dark">
+              <tr>
+                <th>Name</th>
+                <th>Mail</th>
+                <th>Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.data.map((value, key) => (
+                <tr key={key}>
+                  <th>{value.name}</th>
+                  <td>{value.mail}</td>
+                  <td>{value.age}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </Layout>
     </div>
